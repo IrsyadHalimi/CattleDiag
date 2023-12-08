7b3df3a6040e4bc6475a3a11e6beaf9091e38add
kishibee <raflyade9@gmail.com>
2023-12-08 07:11:17 -0800

    layout

---
+++ Mobile Development/CattleDiag/app/src/main/java/com/example/cattlediag/presentation/identify/IdentifyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cattlediag.presentation.identify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cattlediag.R
+
+class IdentifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_identify)
+    }
+}
--- Mobile Development/CattleDiag/app/src/main/java/com/example/cattlediag/presentation/result/ResultActivity.kt
+++ Mobile Development/CattleDiag/app/src/main/java/com/example/cattlediag/presentation/result/ResultActivity.kt
@@ -1,11 +1,12 @@
-package com.example.cattlediag
+package com.example.cattlediag.presentation.result
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cattlediag.R
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result)
     }
 }
